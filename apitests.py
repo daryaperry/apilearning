6efff9356b47da4de35ba4dddb1fd1d86dced95d
Darya <daryape@gmail.com>
2025-05-16 17:48:34 +0300

    first commit

---
+++ apitests.py
@@ -0,0 +1,30 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import requests
+api_key = "30fe13b10441a95ac90adcc9351e8bfd"  # Replace with your actual API key
+api_url = f"https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={api_key}"
+
+city = "New York"
+
+# Make the API request
+response = requests.get(api_url, params={
+    "q": city,
+    "appid": api_key,
+    "units": "metric"  # Use "metric" for Celsius, "imperial" for Fahrenheit
+})
+
+# Check if the request was successful
+if response.status_code == 200:
+    data = response.json()
+    # Extract and print relevant information
+    print(data)
+    temp = data["main"]["temp"]
+    weather = data["weather"][0]["description"]
+    print(f"Current temperature in {city} is {temp}°C with {weather}.")
+    windspeed=data["wind"]["speed"]
+    print(f"current wind speed is {windspeed}.")
+else:
+    print("Failed to fetch weather data:", response.status_code, response.text)
